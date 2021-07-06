4ec4336521f1117572d049f862e8dc0a883c5206
Alexandr <torasov.alexandr@gmail.com>
2021-07-06 19:45:40 +0300

    Hw4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 import { useRoutes } from "./routes";
 
 import { Navbar } from "./components/navBar/navBar";
